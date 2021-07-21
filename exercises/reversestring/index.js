fbca4850a469964435965c291842d83cde8cb1fa
gillyspy <gillyspy@gmail.com>
2021-07-21 13:10:28 -0400

    reversestring implementation with reduce

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,10 +6,26 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
+function reverse1(str) {
 
   const backwardStr = [...str].reverse().join('');
   return backwardStr;
 }
+
+function reverse2(str){
+  const backwardStr =  [...str].reduce( (reduction,value)=>{
+    return value + reduction;
+  });
+
+  return backwardStr;
+}
+
+function reverse3(str){
+
+}
+
+const reverse = reverse2;
+
+
 
 module.exports = reverse;
